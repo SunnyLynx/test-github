fea32ad192fd851d8e9c4aeb4d5c7e864dca76c6
SunnyLynx <SunnyKitten-studio@yandex.ru>
2022-01-20 11:27:49 +0500

    добавлен main.js

---
+++ main.js
@@ -0,0 +1,172 @@
+const option1 = document.querySelector('.option1'),
+      option2 = document.querySelector('.option2'),
+      option3 = document.querySelector('.option3'),
+      option4 = document.querySelector('.option4');
+
+const optionElements = document.querySelectorAll('.option'); 
+
+const question = document.getElementById('question'); 
+
+const numberOfQuestion = document.getElementById('number-of-question'), 
+      numberOfAllQuestions = document.getElementById('number-of-all-questions'); 
+
+let indexOfQuestion, 
+    indexOfPage = 0; 
+
+const answersTraker = document.getElementById('answers-tracker'); 
+const btnNext = document.getElementById('btn-next'); 
+
+let score = 0; 
+
+const correctAnswer = document.getElementById('correct-answer'), 
+      numberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'), 
+      btnTryAgain = document.getElementById('btn-try-again'); 
+
+const questions = [
+    {
+        question: 'Кто лучше видит?',
+        options: [
+            'Филин',
+            'Орел',
+            'Крот',
+            'Я',
+        ],
+        rightAnswer: 1
+    },
+    {
+        question: 'Сколько лапок у паука?',
+        options: [
+            '8',
+            '6',
+            '40',
+            'уже 0',
+        ],
+        rightAnswer: 0
+    },
+    {
+        question: 'Сколько крыльев у стрекозы?',
+        options: [
+            '2',
+            '6',
+            '4',
+            'уже 0',
+        ],
+        rightAnswer: 2
+    }
+];
+
+numberOfAllQuestions.innerHTML = questions.length; 
+
+const load = () => {
+    question.innerHTML = questions[indexOfQuestion].question; 
+    option1.innerHTML = questions[indexOfQuestion].options[0];
+    option2.innerHTML = questions[indexOfQuestion].options[1];
+    option3.innerHTML = questions[indexOfQuestion].options[2];
+    option4.innerHTML = questions[indexOfQuestion].options[3];
+
+    numberOfQuestion.innerHTML = indexOfPage + 1;
+    indexOfPage++; 
+};
+
+let completedAnswers = [];
+
+const randomQuestion = () => {
+    let randomNumber = Math.floor(Math.random() * questions.length); 
+    let hitDuplicate = false;
+
+    if(indexOfPage == questions.length) { 
+        quizOver ()
+    } else {
+        if(completedAnswers.length > 0) { 
+            completedAnswers.forEach(item => { 
+                if(item == randomNumber) { 
+                    hitDuplicate = true;
+                }
+            });
+            if(hitDuplicate) { 
+                randomQuestion();  
+            } else {
+                indexOfQuestion = randomNumber; 
+                load();
+            }
+        }
+        if(completedAnswers.length == 0) { 
+            indexOfQuestion = randomNumber; 
+                load();
+        }
+    }
+    completedAnswers.push(indexOfQuestion); 
+};
+
+const checkAnswer = el => {
+    if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer) { 
+        el.target.classList.add('correct');  
+        updateAnswerTraker('correct');
+        score++; 
+    } else { 
+        el.target.classList.add('wrong');  
+        updateAnswerTraker('wrong');
+    }
+    disableOptions();
+}
+
+for(option of optionElements) {
+    option.addEventListener('click', e => checkAnswer(e));
+}
+
+const disableOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.add('disable'); 
+        if(item.dataset.id == questions[indexOfQuestion].rightAnswer) { 
+            item.classList.add('correct');
+        }
+    })
+}
+
+const enableOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.remove('disable', 'correct', 'wrong');
+    })
+}
+
+const answerTraker = () => {
+    questions.forEach(() => {
+        const div = document.createElement('div'); 
+        answersTraker.appendChild(div); 
+    })
+}
+
+const updateAnswerTraker = stat => {
+    answersTraker.children[indexOfPage - 1].classList.add(`${stat}`); 
+}
+
+const validate = () => { 
+    if(!optionElements[0].classList.contains('disable')) { 
+        alert('Вам нужно выбрать один из вариантов ответа');
+    } else {
+        randomQuestion();
+        enableOptions();
+    }
+}
+
+const quizOver = () => {
+    document.querySelector('.quiz-over-modal').classList.add('active');
+    correctAnswer.innerHTML = score;
+    numberOfAllQuestions2.innerHTML = questions.length;
+};
+
+const tryAgain = () => {
+    window.location.reload(); 
+};
+
+btnTryAgain.addEventListener('click', tryAgain);
+
+btnNext.addEventListener('click', () => {
+    validate();
+}) 
+
+window.addEventListener('load', () => { 
+    randomQuestion();
+    answerTraker();
+});
+
